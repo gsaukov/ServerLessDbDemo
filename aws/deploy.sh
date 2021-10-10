5b5258929249b9399e438a797a5a11fb52979ea7
georgy.saukov <gsaukov@yahoo.com>
2021-10-10 21:54:05 +0200

    lambda sldb inizializer deployment fixes.

--- aws/deploy.sh
+++ aws/deploy.sh
@@ -16,17 +16,28 @@
 # install jq
 #brew install jq
 
-#create s3 bucket and upload java application jar
 echo 'Creating s3 bucket deployment-lambdas-...'
-aws s3 mb s3://my-new-lambda-bucket-6328764287364 --region us-east-1
-echo 'Copying file to s3'
-aws s3 cp ../SLDB/build/libs/SLDB-0.1.0.jar s3://my-new-lambda-bucket-6328764287364/
-aws s3 cp ../SLDB/build/distributions/dependencies.zip s3://my-new-lambda-bucket-6328764287364/
+aws s3 mb s3://my-new-lambda-bucket-6328764287365 --region us-east-1
+
+echo '==================   Copying resource to s3 STARTED   =========================='
+aws s3 cp ../SLDBInitializer/build/libs/SLDBInitializer-0.1.0.jar s3://my-new-lambda-bucket-6328764287365/
+aws s3 cp ../SLDBInitializer/build/distributions/SLDBInitializer-dependencies.zip s3://my-new-lambda-bucket-6328764287365/
+aws s3 cp ../SLDB/build/libs/SLDB-0.1.0.jar s3://my-new-lambda-bucket-6328764287365/
+aws s3 cp ../SLDB/build/distributions/SLDB-dependencies.zip s3://my-new-lambda-bucket-6328764287365/
+
+aws s3 cp ./cf-deploy/cf-network.yaml s3://my-new-lambda-bucket-6328764287365/
+aws s3 cp ./cf-deploy/cf-aurora-serverless.yaml s3://my-new-lambda-bucket-6328764287365/
+echo '==================   Copying resource to s3 COMPLETE    =========================='
 
 # run cloudformation template
 #echo 'Creating stack...'
-#aws cloudformation create-stack --stack-name mynewstack --template-body file://cloud.template --region=us-east-1
+echo '==================   Stack creation STARTED        =========================='
+aws cloudformation create-stack --stack-name cf-network --template-url https://s3.amazonaws.com/my-new-lambda-bucket-6328764287365/cf-network.yaml --region=us-east-1 --parameters ParameterKey=VPCID,ParameterValue=vpc-0677fa4f943931f0b
+aws cloudformation create-stack --stack-name cf-aurora-serverless --template-url https://s3.amazonaws.com/my-new-lambda-bucket-6328764287365/cf-aurora-serverless.yaml --region=us-east-1
 echo '==================   Stack creation COMPLETE        =========================='
+
+#create s3 bucket and upload java application jar
+echo 'Copying file to s3'
 
 #create role for lambda
 echo 'creating role for lambda...'
