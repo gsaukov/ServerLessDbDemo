a7a5ee8426c8542d4d2bd37d1661b48fc7c21f34
georgy.saukov <gsaukov@yahoo.com>
2021-10-10 22:37:23 +0200

    lambda sldb db initializer connection properties.

---
+++ SLDBInitializer/src/main/java/com/sldd/dbinitializer/ConnectionProperties.java
@@ -0,0 +1,69 @@
+package com.sldd.dbinitializer;
+
+public class ConnectionProperties{
+
+	private String dbClusterIdentifier;
+	private String password;
+	private String dbname;
+	private String engine;
+	private int port;
+	private String host;
+	private String username;
+
+	public String getDbClusterIdentifier() {
+		return dbClusterIdentifier;
+	}
+
+	public void setDbClusterIdentifier(String dbClusterIdentifier) {
+		this.dbClusterIdentifier = dbClusterIdentifier;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getDbname() {
+		return dbname;
+	}
+
+	public void setDbname(String dbname) {
+		this.dbname = dbname;
+	}
+
+	public String getEngine() {
+		return engine;
+	}
+
+	public void setEngine(String engine) {
+		this.engine = engine;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	public String getHost() {
+		return host;
+	}
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+}
