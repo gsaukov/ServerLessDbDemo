b21bd7e299d1ecf1d488b88b502320486cf0a6d3
georgy.saukov <gsaukov@yahoo.com>
2021-10-11 23:17:08 +0200

    nested stack deployment automation

--- aws/deploy_v2.sh
+++ aws/deploy_v2.sh
@@ -1,7 +1,7 @@
 # install jq
 #brew install jq
 
-vpc_id=$(aws ec2 describe-vpcs --filters "Name=isDefault, Values=true" | jq '.Vpcs[0].VpcId')
+vpc_id=$(aws ec2 describe-vpcs --filters "Name=isDefault, Values=true" | jq -r '.Vpcs[0].VpcId')
 echo 'Creating stack in VPC: ' ${vpc_id}
 
 echo 'Creating s3 bucket deployment-lambdas-...'
@@ -19,7 +19,10 @@ echo '==================   Copying resource to s3 COMPLETE    ==================
 # run cloudformation template
 #echo 'Creating stack...'
 echo '==================   Stack creation STARTED        =========================='
-aws cloudformation wait stack-create-complete --stack-name cf-network --template-url https://s3.amazonaws.com/my-new-lambda-bucket-6328764287365/cf-network.yaml --region=us-east-1 --parameters ParameterKey=VPCID,ParameterValue=${vpc_id}
-aws cloudformation wait stack-create-complete --stack-name cf-aurora-serverless --template-url https://s3.amazonaws.com/my-new-lambda-bucket-6328764287365/cf-aurora-serverless.yaml --region=us-east-1
-aws cloudformation wait stack-create-complete --stack-name cf-sldb-initializer --template-url https://s3.amazonaws.com/my-new-lambda-bucket-6328764287365/cf-sldb-initializer.yaml --region=us-east-1
+aws cloudformation create-stack --stack-name cf-network --template-url https://s3.amazonaws.com/my-new-lambda-bucket-6328764287365/cf-network.yaml --region=us-east-1 --parameters ParameterKey=VPCID,ParameterValue=$vpc_id
+aws cloudformation wait stack-create-complete --stack-name cf-network
+aws cloudformation create-stack --stack-name cf-aurora-serverless --template-url https://s3.amazonaws.com/my-new-lambda-bucket-6328764287365/cf-aurora-serverless.yaml --region=us-east-1
+aws cloudformation wait stack-create-complete --stack-name cf-aurora-serverless
+aws cloudformation create-stack --stack-name cf-sldb-initializer --template-url https://s3.amazonaws.com/my-new-lambda-bucket-6328764287365/cf-sldb-initializer.yaml --region=us-east-1
+aws cloudformation wait stack-create-complete --stack-name cf-sldb-initializer
 echo '==================   Stack creation COMPLETE        =========================='
