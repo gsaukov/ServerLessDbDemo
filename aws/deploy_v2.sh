4af51015ba01b7e4bd5bf4bcedf43b347c4f00b4
georgy.saukov <gsaukov@yahoo.com>
2021-11-04 21:14:46 +0100

    sldb initializer route table dynamic resolution, proper S3 gateway VPCEndpoint

--- aws/deploy_v2.sh
+++ aws/deploy_v2.sh
@@ -2,7 +2,9 @@
 #brew install jq
 
 vpc_id=$(aws ec2 describe-vpcs --filters "Name=isDefault, Values=true" | jq -r '.Vpcs[0].VpcId')
+route_table_id=$(aws ec2 describe-route-tables --filters "Name=vpc-id,Values=$vpc_id" | jq -r '.RouteTables[0].Associations[0].RouteTableId')
 echo 'Creating stack in VPC: ' ${vpc_id}
+echo 'Using default route table: ' ${route_table_id}
 
 echo 'Creating s3 bucket deployment-lambdas-...'
 aws s3 mb s3://my-new-lambda-bucket-6328764287365 --region us-east-1
@@ -18,8 +20,6 @@ aws s3 cp ./cf-deploy/cf-network.yaml s3://my-new-lambda-bucket-6328764287365/
 aws s3 cp ./cf-deploy/cf-aurora-serverless.yaml s3://my-new-lambda-bucket-6328764287365/
 aws s3 cp ./cf-deploy/cf-sldb-initializer.yaml s3://my-new-lambda-bucket-6328764287365/
 echo '==================   Copying resource to s3 COMPLETE    =========================='
-
-
 
 echo '==================   SLDB_init_lambda_role creation STARTED        =========================='
 aws iam create-role --role-name SLDB_init_lambda_role --assume-role-policy-document file://cf-deploy/trust-policy.json
@@ -41,10 +41,14 @@ echo '==================   Stack creation STARTED        =======================
 aws cloudformation create-stack --stack-name cf-network --template-url https://s3.amazonaws.com/my-new-lambda-bucket-6328764287365/cf-network.yaml \
 --region=us-east-1 --parameters ParameterKey=VPCID,ParameterValue=$vpc_id
 aws cloudformation wait stack-create-complete --stack-name cf-network
+
 aws cloudformation create-stack --stack-name cf-aurora-serverless --template-url https://s3.amazonaws.com/my-new-lambda-bucket-6328764287365/cf-aurora-serverless.yaml \
 --region=us-east-1
 aws cloudformation wait stack-create-complete --stack-name cf-aurora-serverless
+
 aws cloudformation create-stack --stack-name cf-sldb-initializer --template-url https://s3.amazonaws.com/my-new-lambda-bucket-6328764287365/cf-sldb-initializer.yaml \
---capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM --region=us-east-1 --parameters ParameterKey=LambdaExecutionRole,ParameterValue=$lambda_role_arn
+--capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM --region=us-east-1 \
+--parameters [{"ParameterKey":"LambdaExecutionRole","ParameterValue":$lambda_role_arn}, {"ParameterKey":"RouteTableId","ParameterValue":$route_table_id}]
 aws cloudformation wait stack-create-complete --stack-name cf-sldb-initializer
+
 echo '==================   Stack creation COMPLETE        =========================='
